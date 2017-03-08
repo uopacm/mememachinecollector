1bf4effc821ea3d2bbb345ac009d0ddd3253bcf8
Austin Schafer <austin@dhcp-10-15-162-226.wireless.pacific.edu>
2017-03-08 14:23:47 -0800

    Cleanup of reddit main and addition of urlNumber argument

--- reddit/main.py
+++ reddit/main.py
@@ -3,29 +3,27 @@ import praw
 from urllib import request
 
 parser = argparse.ArgumentParser(description='Process some integers.')
-parser.add_argument('arg1', help='1st Argument')
+parser.add_argument('urlNumber', help='Number of URLs to look up')
 args = parser.parse_args()
+
 reddit = praw.Reddit(client_id='B8ZEg7a304hCfw',
 	client_secret='wWoLUJrIAWGbPddy3W0lzU7dVqw',
 	user_agent='meme-machine:reddit-collector:v1.0 (by /u/theMusicalGamer88)')
 
 def main():
-        subreddit = reddit.subreddit('startrekmemes')
-        tenurls = []
-        for submission in subreddit.hot(limit=10):
-                if 'youtube' not in submission.url:
-                        tenurls.append(submission.url)
-        i = 0
+	subreddit = reddit.subreddit('startrekmemes')
+	tenurls = []
+	for i,submission in enumerate(subreddit.hot(limit=int(args.urlNumber))):
+		if 'youtube' not in submission.url:
+			tenurls.append(submission.url)
 
-        for url in tenurls:
-                print(url)
+	for i,url in enumerate(tenurls):
+		print(url)
 
-                if '.jpg' in url:
-                        request.urlretrieve(url, 'image.' + str(i) + '.jpg')
-                elif '.png' in url:
-                        request.urlretrieve(url, 'image.' + str(i) + '.png')
-                
-                i += 1
+		if '.jpg' in url:
+			request.urlretrieve(url, 'image.' + str(i) + '.jpg')
+		elif '.png' in url:
+				request.urlretrieve(url, 'image.' + str(i) + '.png')
 
 if __name__ == "__main__":
 	main()
