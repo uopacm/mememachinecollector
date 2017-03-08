b8b124b63a5019230d47f1b12a84d23c4b44f463
theMusicalGamer <pianojammer@comcast.net>
2017-03-07 23:10:13 -0800

    Update main.py
    Adding beginning PRAW stuff (please don't share the client_id or client_secret fields)

--- reddit/main.py
+++ reddit/main.py
@@ -1,9 +1,12 @@
 import argparse
+import praw
 
 parser = argparse.ArgumentParser(description='Process some integers.')
 parser.add_argument('arg1', help='1st Argument')
 args = parser.parse_args()
-
+reddit = praw.Reddit(client_id='B8ZEg7a304hCfw',
+	client_secret='wWoLUJrIAWGbPddy3W0lzU7dVqw',
+	user_agent='meme-machine:reddit-collector:v1.0 (by /u/theMusicalGamer88)')
 
 if __name__ == "__main__":
 	pass 
