33042126ea6a399ea5b3aa851bcfda326a2c4304
theMusicalGamer <pianojammer@comcast.net>
2017-03-08 10:48:55 -0800

    Added the routine for gathering memes
    Using r/startrekmemes as a test (because I know it has memes) while
    filtering out YouTube videos as I don't want to try and download those

--- reddit/main.py
+++ reddit/main.py
@@ -1,5 +1,6 @@
 import argparse
 import praw
+from urllib import request
 
 parser = argparse.ArgumentParser(description='Process some integers.')
 parser.add_argument('arg1', help='1st Argument')
@@ -8,5 +9,23 @@ reddit = praw.Reddit(client_id='B8ZEg7a304hCfw',
 	client_secret='wWoLUJrIAWGbPddy3W0lzU7dVqw',
 	user_agent='meme-machine:reddit-collector:v1.0 (by /u/theMusicalGamer88)')
 
+def main():
+        subreddit = reddit.subreddit('startrekmemes')
+        tenurls = []
+        for submission in subreddit.hot(limit=10):
+                if 'youtube' not in submission.url:
+                        tenurls.append(submission.url)
+        i = 0
+
+        for url in tenurls:
+                print(url)
+
+                if '.jpg' in url:
+                        request.urlretrieve(url, 'image.' + str(i) + '.jpg')
+                elif '.png' in url:
+                        request.urlretrieve(url, 'image.' + str(i) + '.png')
+                
+                i += 1
+
 if __name__ == "__main__":
-	pass 
+	main()
