57fdc5e18e1ed65159ab2a78bf1dfec4d2fbd609
Austin Schafer <austin@dhcp-10-15-162-226.wireless.pacific.edu>
2017-03-07 18:15:39 -0800

    Template Python Mains for each collector

---
+++ tumblr/main.py
@@ -0,0 +1,9 @@
+import argparse
+
+parser = argparse.ArgumentParser(description='Process some integers.')
+parser.add_argument('arg1', help='1st Argument')
+args = parser.parse_args()
+
+
+if __name__ == "__main__":
+	pass 
