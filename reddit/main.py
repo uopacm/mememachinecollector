972406538053ff3f1af5b628b8a8a599091df4b8
theMusicalGamer <pianojammer@comcast.net>
2017-03-08 18:08:13 -0800

    Added "getRandomSubreddit" method
    This adds functionality to get the same amount of memes from a random
    subreddit that has "memes" or "meme" in it's display_name variable (not
    title, that's what appears as the title for the page). Also, added the
    data folder to the .gitignore

--- reddit/main.py
+++ reddit/main.py
@@ -3,6 +3,7 @@ import praw
 import os
 import time
 from urllib import request
+import random
 
 parser = argparse.ArgumentParser(description='Fetch URLs of reddit images')
 parser.add_argument('urlNumber', type=int, help='Number of URLs to look up per subreddit')
@@ -26,6 +27,9 @@ def main():
 	#for every subreddit, get Images
 	for subreddit in subreddits:
 		getImagesFromSubreddit(subreddit)
+	
+	subreddit = getRandomSubreddit(subreddits)
+	getImagesFromSubreddit(subreddit)
 
 def getImagesFromSubreddit(subreddit):
 	for i,submission in enumerate(subreddit.hot(limit=int(args.urlNumber))):
@@ -39,10 +43,27 @@ def getImagesFromSubreddit(subreddit):
 
 		# Fetch Images
 		print(url)
-		if '.jpg' in url:
+		'''if '.jpg' in url:
 			request.urlretrieve(url, '{}/image{}.jpg'.format(args.saveDirectory,i))
 		elif '.png' in url:
-				request.urlretrieve(url, '{}/image{}.png'.format(args.saveDirectory,i))
+			request.urlretrieve(url, '{}/image{}.png'.format(args.saveDirectory,i))'''
+		if '.jpg' in url:
+			request.urlretrieve(url, '{}/{}.jpg'.format(args.saveDirectory,submission.id))
+		elif '.png' in url:
+			request.urlretrieve(url, '{}/{}.png'.format(args.saveDirectory,submission.id))
+
+def getRandomSubreddit(subreddits):
+	list = reddit.subreddits.search_by_topic('memes')
+	randNum = random.randint(0,len(list) - 1)
+	
+	while 'memes' not in list[randNum].display_name and list[randNum] in subreddits:
+		randNum = random.randint(0,len(list) - 1)
+		
+		if 'memes' in list[randNum].display_name or 'meme' in list[randNum].display_name:
+			break
+	
+	print(list[randNum].display_name)
+	return list[randNum]
 
 if __name__ == "__main__":
 	# Create the requested directory if it does not exist
