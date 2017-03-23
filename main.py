65e1276fb8fb4bc09822a1126cb44b09bdf85041
theMusicalGamer <pianojammer@comcast.net>
2017-03-23 15:29:11 -0700

    Fixed a thing
    Made a universal Python dictionary that the Reddit and Imgur APIs will
    feed into instead of gathering the Reddit dictionary and downloading
    from it and then gathering the Imgur dictionary and downloading from it.

--- main.py
+++ main.py
@@ -2,24 +2,33 @@ from reddit.python import redditMemes
 from imgur.python import imgurMemes
 import json
 from urllib import request
+import time
 
 parser = argparse.ArgumentParser(description='Save memes in a directory')
 parser.add_argument('saveDirectory', type=str, help='Directory to save images in')
 args = parser.parse_args()
 
 def main():
+	memeDict = {
+		'memes': []
+	}
 	# Receive from Reddit
 	reddit = redditMemes.getMemes()
 	redditJson = json.loads(reddit)
-	download(redditJson.get('memes'))
+	for meme in redditJson.get('memes'):
+		memeDict.get('memes').append(meme)
 	
 	# Receive from Imgur
 	imgur = imgurMemes.getMemes()
 	imgurJson = json.loads(imgur)
-	download(imgurJson.get('memes'))
+	for meme in imgurJson.get('memes'):
+		memeDict.get('memes').append(meme)
+	
+	download(memeDict.get('memes'))
 
 def download(memeList):
 	for meme in memeList:
+		time.sleep(1)
 		print('Downloading ' + meme.get('url') + '...')
 		
 		if '.jpg' in url:
